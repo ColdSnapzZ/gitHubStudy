68ec626533009acf8451cec5828fd754bd62fe90
torain <2430067402@qq.com>
2020-12-24 15:15:29 +0800

    hello

--- src/main/java/com/example/spring_git/SpringGitApplication.java
+++ src/main/java/com/example/spring_git/SpringGitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringGitApplication {
 
     public static void main(String[] args) {
+        System.out.println("999");
         SpringApplication.run(SpringGitApplication.class, args);
     }
 
