b4c6936a6c6461f116cdbbd115c4d4d4566aa423
torain <2430067402@qq.com>
2020-12-24 15:30:17 +0800

    zht

--- src/main/java/com/example/spring_git/SpringGitApplication.java
+++ src/main/java/com/example/spring_git/SpringGitApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringGitApplication {
 
     public static void main(String[] args) {
-        System.out.println("999");
-        System.out.println("2333");
         SpringApplication.run(SpringGitApplication.class, args);
     }
 
---
+++ src/main/java/com/example/spring_git/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.spring_git.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author zht
+ * @Date 2020/12/24 15:27
+ * @Description
+ */
+@RequestMapping("user")
+public class UserController {
+
+    @RequestMapping("getUser")
+    @ResponseBody
+    public String getUser(){
+        return "user";
+    }
+}
